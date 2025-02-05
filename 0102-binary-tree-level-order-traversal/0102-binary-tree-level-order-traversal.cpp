9322952dfcea7a871621b5c7910d8eae10437b6c
alisha027 <119072382+alisha027@users.noreply.github.com>
2025-02-06 00:37:12 +0530

    Time: 0 ms (100%), Space: 17.2 MB (43.77%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         vector<vector<int>> temp;
+//         queue<TreeNode*> q;
+//         if(root==NULL){
+//             return temp;
+//         }
+//         q.push(root);
+//         while(!q.empty()){
+//             vector<int> temp2;
+//             for(int i=0;i<q.size();i++){
+//                 TreeNode* newnode = q.front();
+//                 q.pop();
+//                 temp2.push_back(newnode->val);
+//                 if(newnode->left!=NULL){
+//                     q.push(newnode->left);
+//                 }
+//                 if(newnode->right!=NULL){
+//                     q.push(newnode->right);
+//                 }
+//             }
+//             temp.push_back(temp2);   
+//         }
+//         return temp;
+//     }
+// };
+vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if(!root){
+            return result;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            vector<int> temp;
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                TreeNode* newnode = q.front();
+                q.pop();
+                temp.push_back(newnode->val);
+                if(newnode->left){
+                    q.push(newnode->left);
+                }
+                if(newnode->right){
+                    q.push(newnode->right);
+                }
+            }
+            result.push_back(temp);
+        } 
+        return result;
+}
+};
