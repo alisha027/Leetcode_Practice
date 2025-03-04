f258745b1dca24162afe57c277eb3838955246c1
alisha027 <119072382+alisha027@users.noreply.github.com>
2025-03-04 23:46:08 +0530

    Time: 0 ms (100%), Space: 21.7 MB (73.87%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> temp;
+        for(int i : asteroids){
+            bool d = false;
+            while(!temp.empty() && temp.back() > 0 && i < 0){
+                if(abs(temp.back()) < abs(i)){
+                    temp.pop_back();
+                    continue;
+                }
+                else if(abs(temp.back())==abs(i)){
+                    temp.pop_back();
+                }
+                    d = true;
+                    break;
+            }
+            if(!d){
+                temp.push_back(i);
+            }
+        }
+        return temp;
+    }
+};
