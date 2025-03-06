ac8ecdaf7f381548ef6fc9ceae62ff9c445a2bff
alisha027 <119072382+alisha027@users.noreply.github.com>
2025-03-06 10:52:23 +0530

    Time: 0 ms (100%), Space: 9.3 MB (39.56%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,19 +1,3 @@
-// class Solution {
-// public:
-//     void lettercomb(string &digits,int index,string temp,vector<string> &ans,vector<string> &pos){
-//         if(digits.size()==)
-//     }
-//     vector<string> letterCombinations(string digits) {
-//         if(digits.length()==0){
-//             return {};
-//         }
-//     }   
-//     vector<string> pos = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-//     vector<string> ans;
-//     lettercomb(digits,0,"",ans,pos);
-//     return ans;
-//     }
-// };
 class Solution {
 public:
     void lettercomb(string &digits, int index, string temp, vector<string> &ans, vector<string> &pos) {
@@ -21,7 +5,6 @@ public:
             ans.push_back(temp);
             return;
         }
-
         string letters = pos[digits[index] - '0'];  
         for (char ch : letters) {  
             lettercomb(digits, index + 1, temp + ch, ans, pos);  
@@ -37,3 +20,4 @@ public:
         return ans;
     }
 };
+
