fb27ee115f8c155f023ef42fbae62d1641b60bfd
alisha027 <119072382+alisha027@users.noreply.github.com>
2025-06-24 10:36:22 +0530

    Time: 4 ms (53.08%), Space: 25.8 MB (20.59%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<int> rows[9];
+        unordered_set<int> cols[9];
+        unordered_set<int> submat[9];
+
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                int val = board[i][j];
+                if(val=='.'){
+                    continue;
+                }
+                if(rows[i].find(val)!=rows[i].end()){
+                    return false;
+                }
+                rows[i].insert(val);
+                if(cols[j].find(val)!=cols[j].end()){
+                    return false;
+                }
+                cols[j].insert(val);
+                int index = (i/3)*3 + j/3;
+                if(submat[index].find(val)!=submat[index].end()){
+                    return false;
+                }
+                submat[index].insert(val);
+            }
+        }
+        return true;
+    }
+};
